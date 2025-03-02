20cca786bc579169ab1058a9f38430d1e5952627
Todd V. Rovito <rovitotv@gmail.com>
2025-03-02 13:23:52 -0500

    - added a check in the test to make sure background tasks deleted the   files

--- tests/test_01_arguments.py
+++ tests/test_01_arguments.py
@@ -3,6 +3,7 @@ import subprocess
 import shlex
 import os
 from pathlib import Path
+import time
 
 '''
 curl -X POST "http://localhost:8000/api/process-tax-form"   -H "accept: application/json"   -H "Content-Type: multipart/form-data"   -F "config_file=@bob_student_example.json"   -F "pdf_form=@/home/rovitotv/code/taxes/2024/f1040_blank.pdf" --output processed_form.pdf
@@ -17,6 +18,15 @@ def test_perfect_arguments():
         file_path = Path("/workspace/temp/processed_form.pdf")
         assert file_path.exists(), f"Output file {file_path} does not exist"
         file_path.unlink()
+        # Check to make sure the background tasks removed the job_dir
+        time.sleep(2)
+        job_directory = Path('/workspace/temp/uploads')
+        for file_path in job_directory.glob('**/*'):
+            if file_path.is_file():
+                pytest.fail(f"There should be no files in the {job_directory} but the file {file_path} exists")
+            elif file_path.is_dir():
+                pytest.fail(f"There should be no directories in the {job_directory} but the directory {file_path} exists")
+        
     except subprocess.CalledProcessError as e:
         print(f"Command failed with return code {e.returncode}")
         print(f"Stdout: {e.stdout}")  # If capture_output was True
