ae4f4d4c8e3668492d98e204d91e7d4136351e47
Todd V. Rovito <rovitotv@gmail.com>
2025-03-01 23:22:20 -0500

    - removed code that is no longer being used

--- opentaxliberty.py
+++ opentaxliberty.py
@@ -102,18 +102,6 @@ def get_widgets():
                 fields.append(annot)
     return fields
     
-def fill_header():
-    write_field_pdf("f1_04[0]", "Bob P")
-    write_field_pdf("f1_05[0]", "Smith")
-    write_field_pdf("f1_06[0]", "222         22         2222")
-    write_field_pdf("f1_10[0]", "9370 Liberty Court")
-    write_field_pdf("f1_12[0]", "Dayton")
-    write_field_pdf("f1_13[0]", "OH")
-    write_field_pdf("f1_13[0]", "45468")
-    write_field_pdf("c1_3[0]", "/1")   # filing Status = Single
-    #write_field_pdf("c1_5[0]", "/1")   # digital assets
-    write_field_pdf("c1_6[0]", "/1")   # someone can claim you as a dependent
-
 def process_input_json(input_json_data: Dict[str, Any], writer):
     for key in input_json_data:
         if key == "configuration":
@@ -132,12 +120,6 @@ def process_input_json(input_json_data: Dict[str, Any], writer):
             write_field_pdf(writer, input_json_data[key]['tag'], ssn_output)
         else:
             write_field_pdf(writer, input_json_data[key]['tag'], input_json_data[key]['value'])
-
-def check_arguments(input_json_file_name: str):
-    input_json_path = Path(input_json_file_name)
-    if input_json_path.exists() == False:
-        print(f"Error: input_json argument does not exist: {input_json_path}", file=sys.stderr)
-        sys.exit(3)
 
 def parse_and_validate_input_json(input_json_file_name: str, 
         pdf_template_file_name: str, job_dir: str) -> Dict[str, Any]:
@@ -222,28 +204,3 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-'''
-# this is the original program before it was a fastapi application
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()                                          
-    parser.add_argument("input_json", help="input file in json format to process")
-    args = parser.parse_args()    
-    check_arguments(args.input_json)
-    input_json_data = parse_and_validate_input_json(args.input_json)
-
-    reader = PdfReader(input_json_data["configuration"]["template_1040_pdf"])
-    writer = PdfWriter()
-
-    page = reader.pages[0]
-    #fields = reader.get_form_text_fields()
-    fields = reader.get_fields()
-    keys_list = list(fields.keys())
-    fields_widgets = get_widgets()
-    #print(fields)
-    writer.append(reader)
-    process_input_json(input_json_data)
-    #fill_header()
-
-    with open(input_json_data["configuration"]["output_file_name"], "wb") as output_stream:
-        writer.write(output_stream)
-'''
