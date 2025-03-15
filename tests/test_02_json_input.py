e1135091a4bc289708396b20d10bf8acb721c4d6
Todd V. Rovito <rovitotv@gmail.com>
2025-03-15 16:59:21 -0400

    - test_03 is failing....I am not sure why

--- tests/test_02_json_input.py
+++ tests/test_02_json_input.py
@@ -21,7 +21,7 @@ def test_bad_json():
         print(f"Error: error creating file: {e}") 
     
     try:
-        command_string = 'curl -v "http://mse-8:8000/api/process-tax-form"   -H "accept: application/json"   -H "Content-Type: multipart/form-data"   -F "config_file=@bad_json.json"   -F "pdf_form=@/workspace/code/taxes/2024/f1040_blank.pdf" --output /workspace/temp/processed_form.pdf'
+        command_string = 'curl -v "http://mse-8:8000/api/process-tax-form" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "config_file=@bad_json.json" -F "W_2_config_file=@../bob_student_W-2.json" -F "pdf_form=@/workspace/code/taxes/2024/f1040_blank.pdf" --output /workspace/temp/processed_form.pdf'
         command_list = shlex.split(command_string)                              
         result = subprocess.run(command_list,                                   
                 capture_output=True, text=True, check=True)
@@ -41,6 +41,46 @@ def test_bad_json():
         print(f"Command failed with return code {e.returncode}")
         print(f"Stdout: {e.stdout}")  # If capture_output was True
         print(f"Stderr: {e.stderr}")  # If capture_output was True
+        assert e.returncode == 4
+
+def test_bad_w2_json():
+    # Create a bad W-2 JSON file
+    bad_w2_json_block = """
+    {
+        "configuration": {
+            "tax_year": 2024,
+            "form": "W-2
+        }
+    }
+    """
+
+    try:
+        with open("bad_w2_json.json", 'w') as f:
+            f.write(bad_w2_json_block)
+    except Exception as e:
+        print(f"Error: error creating file: {e}")
+    
+    try:
+        command_string = 'curl -v "http://mse-8:8000/api/process-tax-form" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "config_file=@../bob_student_F1040.json" -F "W_2_config_file=@bad_w2_json.json" -F "pdf_form=@/workspace/code/taxes/2024/f1040_blank.pdf" --output /workspace/temp/processed_form.pdf'
+        command_list = shlex.split(command_string)
+        result = subprocess.run(command_list,
+                capture_output=True, text=True, check=True)
+        os.remove("bad_w2_json.json")
+        assert "HTTP/1.1 400 Bad Request" in result.stderr, "result code of 400 was not found in curl output"
+        
+        # check to make sure the background tasks removed the job_dir
+        time.sleep(2)
+        job_directory = Path('/workspace/temp/uploads')
+        for file_path in job_directory.glob('**/*'):
+            if file_path.is_file():
+                pytest.fail(f"There should be no files in the {job_directory} but the file {file_path} exists")
+            elif file_path.is_dir():
+                pytest.fail(f"There should be no directories in the {job_directory} but the directory {file_path} exists")
+                
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+        print(f"Stdout: {e.stdout}")
+        print(f"Stderr: {e.stderr}")
         assert e.returncode == 4
 
 '''
