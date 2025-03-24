4be223e1bcd78dcacf0d3252ed3d75d9290a3b0d
Todd V. Rovito <rovitotv@gmail.com>
2025-03-23 23:58:45 -0400

    - added messages that show which parts of the tests are running

--- tests/start_tests.sh
+++ tests/start_tests.sh
@@ -3,11 +3,13 @@
 # Shell script to start the testing container, export variables, and execute
 # the tests
 
+echo "Starting mypy....."
 podman run -it --rm \
 	--mount type=bind,source=$HOME,target=/workspace \
 	opentaxliberty:20250316 \
 	sh -c "cd /workspace/code/qualia_insights/opentaxliberty/tests && mypy .." 
 
+echo "Starting pytest...."
 podman run -it --rm \
 	--mount type=bind,source=$HOME,target=/workspace \
 	opentaxliberty:20250316 \
